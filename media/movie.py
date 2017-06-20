543a1709f6da4dcf6dbbf9934848e1c9a7889b3e
pallvigoel <goelpallvi21@gmail.com>
2017-06-20 14:01:41 +0530

    Update movie.py

--- media/movie.py
+++ media/movie.py
@@ -1,11 +1,25 @@
 import webbrowser
 
-"""This Docstring what the movie class does"""
+
 
 
 class Movie():
-    """This Docstring explains constructor methodand its input"""
+    """This class represents the blueprint of a movie"""
+   
     def __init__(self, title, story, image, trailer):
+         """This method  creates a movie
+       :param title: title
+       :title: str
+       
+       :param story: storyline
+       :title: str
+       
+       :param image: poster_image_url
+       :title: str
+       
+       param trailer: trailer_youtube_url
+       :title: str
+       """
         self.title = title
         self.storyline = story
         self.poster_image_url = image
