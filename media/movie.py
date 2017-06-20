e31762db92745b6044d0e7ee3bd921c7da265517
pallvigoel <goelpallvi21@gmail.com>
2017-06-20 14:10:24 +0530

    Update movie.py

--- media/movie.py
+++ media/movie.py
@@ -1,25 +1,24 @@
 import webbrowser
 
 
-
-
 class Movie():
     """This class represents the blueprint of a movie"""
-   
+
     def __init__(self, title, story, image, trailer):
-         """This method  creates a movie
-       :param title: title
-       :title: str
-       
-       :param story: storyline
-       :title: str
-       
-       :param image: poster_image_url
-       :title: str
-       
-       param trailer: trailer_youtube_url
-       :title: str
-       """
+        """This method  creates a movie
+
+        :param title: title
+        :type title: str
+
+        :param story: storyline
+        :type story: str
+
+        :param image: poster_image_url
+        :type image: str
+
+        param trailer: trailer_youtube_url
+        :type trailer: str
+        """
         self.title = title
         self.storyline = story
         self.poster_image_url = image
