9e34775121fc515d5479b2fd2cd12ef049dffd1a
pallvigoel <goelpallvi21@gmail.com>
2017-06-20 07:32:27 +0530

    Add files via upload

---
+++ media/movie.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+"""This Docstring what the movie class does"""
+
+
+class Movie():
+    """This Docstring explains constructor methodand its input"""
+    def __init__(self, title, story, image, trailer):
+        self.title = title
+        self.storyline = story
+        self.poster_image_url = image
+        self.trailer_youtube_url = trailer
